342ec11f1e531549468c9637c7e30b7d59695302
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2022-10-27 16:55:17 +0300

    first step

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>second commit</p>
+        <p>1-first-step</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
