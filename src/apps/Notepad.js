38ea7013ed0293e86d3a0c173d2bf2c6b12fa3c9
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2022-12-24 20:10:13 +0200

    add text area in notepad

--- src/apps/Notepad.js
+++ src/apps/Notepad.js
@@ -8,7 +8,31 @@ function Notepad() {
 
   return (
     <div className='main-container-notepad'>
-        Notepad component
+      <div className='notepad-header'>
+        <div className='btn-notepad'>
+            File
+        </div>
+        <div className='btn-notepad'>
+            Edit
+        </div>
+        <div className='btn-notepad'>
+            Search
+        </div>
+        <div className='btn-notepad'>
+            Help
+        </div>
+      </div>
+      <div className='working-area-notepad'>
+        <textarea className='text-area'>At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.</textarea>
+        <div className='scroll'>
+          <div className='scroll-btn'>
+            <div className='arrow-btn-top'></div>
+          </div>
+          {/* <div className='scroll-btn scroll-bottom'>
+            <div className='arrow-btn-bottom'></div>
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 }
