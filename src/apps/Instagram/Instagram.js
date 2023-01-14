8832adff90d777cf10d476cc621370073a1a0b4c
kozakevich13 <hanibalsmitti@gmail.com>
2023-01-14 21:51:12 +0200

    add instagram app

---
+++ src/apps/Instagram/Instagram.js
@@ -0,0 +1,18 @@
+import './Instagram.css';
+
+
+function Instagram() {
+  
+  return (
+    <div className='main-container-notepad'>
+      <div className='notepad-header'>
+     
+      </div>
+      <div className='working-area-notepad'>
+       
+      </div>
+    </div>
+  );
+}
+
+export default Instagram;
